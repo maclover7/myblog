fcb2ba4175efaa6d3442e8be8537fa716ae82349
maclover7 <me@jonathanmoss.me>
2015-07-12 00:03:25 -0400

    Publisher script update

--- publisher.sh
+++ publisher.sh
@@ -1,1 +1,9 @@
+jekyll build
+
+s3cmd del --recursive s3://www.sparklingcode.co/about --verbose
+s3cmd del --recursive s3://www.sparklingcode.co/css --verbose
+s3cmd del --recursive s3://www.sparklingcode.co/jekyll --verbose
+s3cmd del s3://www.sparklingcode.co/feed.xml --verbose
+s3cmd del s3://www.sparklingcode.co/index.html --verbose
+
 s3cmd sync --delete-removed _site/ s3://www.sparklingcode.co --verbose
